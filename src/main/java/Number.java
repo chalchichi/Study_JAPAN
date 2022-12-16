935c50a29c72f3b15efb3988e1f03606188d9435
ohyunhu <dhdbsgn111@naver.com>
2022-12-16 18:28:01 +0900

    추가

--- src/main/java/Number.java
+++ src/main/java/Number.java
@@ -6,12 +6,56 @@ public class Number implements Japaness{
     public HashMap<String, String> getword()
     {
         HashMap<String, String> word = new HashMap<>();
-        word.put("僕","보쿠");
-        word.put("私","와타시");
-        word.put("俺","오레");
-        word.put("君","키미");
-        word.put("誰","타레");
+        word.put("いち","1");
+        word.put("に","2");
+        word.put("さん","3");
+        word.put("よん","4");
+        word.put("ご","5");
+        word.put("ろく","6");
+        word.put("なな","7");
+        word.put("はち","8");
+        word.put("きゅう","9");
+        word.put("じゅう","10");
 
+        word.put("にじゅう","20");
+        word.put("さんじゅう","30");
+        word.put("よんじゅう","40");
+        word.put("ごじゅう","50");
+        word.put("ろくじゅう","60");
+        word.put("ななじゅう","70");
+        word.put("はちじゅう","80");
+        word.put("きゅうじゅう","90");
+
+        word.put("ひゃく","100");
+        word.put("にひゃく","200");
+        word.put("さんびゃく","300");
+        word.put("よんひゃく","400");
+        word.put("ごひゃく","500");
+        word.put("ろっぴゃく","600");
+        word.put("ななひゃく","700");
+        word.put("はっぴゃく","800");
+        word.put("きゅうひゃく","900");
+
+        word.put("せん","1000");
+        word.put("にせん","2000");
+        word.put("さんぜん","3000");
+        word.put("よんせん","4000");
+        word.put("ごせん","5000");
+        word.put("ろくせん","6000");
+        word.put("ななせん","7000");
+        word.put("はっせん","8000");
+        word.put("きゅうせん","9000");
+
+        word.put("ひとつ","하나");
+        word.put("ふたつ","둘");
+        word.put("みっつ","셋");
+        word.put("よっつ","넷");
+        word.put("いつつ","다섯");
+        word.put("むっつ","여섯");
+        word.put("ななつ","일곱");
+        word.put("やっつ","여덟");
+        word.put("ここのつ","아홉");
+        word.put("とお","열");
         return word;
     }
 }
